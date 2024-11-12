091c7759fc3f2685159f5808fec3504ff7b5a80d
韩高钶 <hangaoke@corp.netease.com>
2024-11-12 14:19:55 +0800

    feat: 添加按钮

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import { View, Text, Button } from '@tarojs/components'
+import Taro, { useLoad } from '@tarojs/taro'
+
 import './index.less'
 
 export default function Index () {
@@ -7,9 +8,17 @@ export default function Index () {
     console.log('Page loaded.')
   })
 
+  const hanldeClick = () => {
+    Taro.showToast({
+      title: '点击了按钮',
+      icon: 'none'
+    })
+  }
+
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+      <Button onClick={hanldeClick}>第一个按钮</Button>
     </View>
   )
 }
